458da06ed1d44724e11f5f50d4362fc66eb0d938
jacoobanderson <j00cwa@gmail.com>
2022-01-27 22:41:54 +0100

    Create basic server setup

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import { connectDB } from './config/mongoose.js'
+import expressLayouts from 'express-ejs-layouts'
+import session from 'express-session'
 import helmet from 'helmet'
 import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 try {
   await connectDB()
+
   const app = express()
 
-  app.use(helmet())
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+  const baseURL = process.env.BASE_URL || '/'
+
   app.use(logger('dev'))
+
+  app.set('view engine', 'ejs')
+  app.set('views', join(directoryFullName, 'views'))
+  app.use(expressLayouts)
+  app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+
+  app.use(express.urlencoded({ extended: false }))
 
   app.listen(process.env.PORT, () => {
     console.log(`Server running at http://localhost:${process.env.PORT}`)
